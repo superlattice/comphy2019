3035541580586d0da5b002d1de71b28dccf5e829
Jeil Jung <superlattice@users.noreply.github.com>
2019-04-16 10:43:39 +0900

    Create exmaple4.12.py

---
+++ exmaple4.12.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+## example4_12
+from polyRoots import *
+import numpy as np
+c = np.array([-250.0,155.0,-9.0,-5.0,1.0])
+print("Roots are:\n",polyRoots(c))
+input("Press return to exit")
